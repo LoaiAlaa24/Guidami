a2173ec78f45c6fc562c5ea2f03a8a3b9c0d597a
mash197 <mohamedashraf8997@gmail.com>
2018-08-06 17:19:06 +0200

    added register page

--- js/Routers/MainStackRouter.js
+++ js/Routers/MainStackRouter.js
@@ -3,6 +3,7 @@ import Login from "../components/login/";
 import Home from "../components/home/";
 import BlankPage from "../components/blankPage";
 import BlankPage2 from "../components/blankPage2";
+import Register from "../components/register";
 import HomeDrawerRouter from "./HomeDrawerRouter";
 import { StackNavigator } from "react-navigation";
 import { Header, Left, Button, Icon, Body, Title, Right } from "native-base";
@@ -14,5 +15,7 @@ export default (StackNav = StackNavigator({
   Login: { screen: Login },
   Home: { screen: Home },
   BlankPage2: { screen: BlankPage2 },
+  
+  Register: { screen: Register },
   
 }));
